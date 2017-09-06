f681627d9e41ec0e7c1ce2b80ebfc38408772755
Alexander <30376452+doograssen@users.noreply.github.com>
2017-09-07 00:46:15 +0300

    Модули

--- js/map.js
+++ js/map.js
@@ -1,17 +1,17 @@
 'use strict';
 
-/* --------------------------------------------------------------------------------------------------------------------
-*   Вызов функци-сеятеля пинов
-* -------------------------------------------------------------------------------------------------------------------*/
-/* размещение DOM-элементов на странице */
-var fragment = document.createDocumentFragment();
-var ownersAmount = window.pin.ownersInfoArray.length;
-for (var i = 0; i < ownersAmount; i++) {
-  fragment.appendChild(window.pin.createPinDomElement(window.pin.ownersInfoArray[i], i));
-}
-var defaultPin = document.querySelector('.pin__main');
-document.querySelector('.tokyo__pin-map').insertBefore(fragment, defaultPin);
-window.pin.addAllPinListeners();
-window.card.addDialogListener();
-
-
+(function () {
+  /* --------------------------------------------------------------------------------------------------------------------
+  *   Вызов функци-сеятеля пинов
+  * -------------------------------------------------------------------------------------------------------------------*/
+  /* размещение DOM-элементов на странице */
+  var fragment = document.createDocumentFragment();
+  var ownersAmount = window.pin.ownersInfoArray.length;
+  for (var i = 0; i < ownersAmount; i++) {
+    fragment.appendChild(window.pin.createPinDomElement(window.pin.ownersInfoArray[i], i));
+  }
+  var defaultPin = document.querySelector('.pin__main');
+  document.querySelector('.tokyo__pin-map').insertBefore(fragment, defaultPin);
+  window.pin.addAllPinListeners();
+  window.card.addDialogListener();
+})();
